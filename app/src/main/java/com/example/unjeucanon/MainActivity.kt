4ecc503db270c0dead11ddc0c3604ddc0f5fec8c
ujju16 <jul.darrouza@gmail.com>
2022-09-22 05:15:03 +0200

    finalisation du jeu

---
+++ app/src/main/java/com/example/unjeucanon/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.unjeucanon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var canonView: CanonView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        canonView = findViewById<CanonView>(R.id.vMain)
+    }
+    override fun onPause() {
+        super.onPause()
+        canonView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        canonView.resume()
+    }
+}
